fede74ab3a44290d02eb56bc714aa8e25520cf72
Jimmy Karlsson <jimmy.viktor.karlsson@gmail.com>
2018-12-03 08:29:53 +0100

    advent of code

---
+++ AdventOfCode2018/Day/Day1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2018.Day
+{
+    class Day1
+    {
+        private List<string> input = new List<string>();
+
+        
+        public Day1()
+        {
+
+        }
+
+        public int GetResultingFrequenzy()
+        {
+            int frequenzy = 0;
+
+            foreach (var i in input)
+            {
+                frequenzy += Convert.ToInt32
+            }
+
+            return frequenzy;
+        }
+
+        private 
+    }
+}
---
+++ AdventOfCode2018/Program.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode2018.Day;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1 day = new Day1();
+            int frequenzy = day.GetResultingFrequenzy();
+            Console.WriteLine(frequenzy);
+
+            Console.ReadLine();
+        }
+    }
+}
