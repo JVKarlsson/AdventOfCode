0da34929b83bbddbfa509fa9454022bf1a5b758a
Jimmy Karlsson <jimmy.karlsson@cgi.com>
2019-12-01 09:07:34 +0100

    Day1 complete

---
+++ AdventOfCode2019/Day/Day1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2019.Day
+{
+    class Day1
+    {
+        private string path = "";
+        public Day1(string p)
+        {
+            path = Path.Combine(p, "InputDay1.txt");
+            var part1 = PartOne();
+            var part2 = PartTwo();
+            Console.WriteLine("Advent of Code Day 1 part 1 : Fuel Sum = " + PartOne());
+            Console.WriteLine("Advent of Code Day 1 part 2 : Fuel Sum = " + PartTwo());
+        }
+
+        private int PartOne()
+        {
+            var sum = File.ReadAllLines(path).ToList().Sum(x => (int.Parse(x) / 3) - 2);
+            return sum;
+        }
+
+        private int PartTwo()
+        {
+            var sum = File.ReadAllLines(path).ToList().Sum(x => GetFuelAndSubFuel(int.Parse(x)));
+            return sum;
+        }
+
+        private int GetFuelAndSubFuel(int fuel)
+        {
+            var amount = Math.Max(0, ((fuel / 3) - 2));
+            if (amount > 0)
+            {
+                amount += GetFuelAndSubFuel(amount);
+            }
+            return amount;
+        }
+
+    }
+}
