0d917f40ce682702083fb14c45e4ab8d72793c2b
Jimmy Karlsson <jimmy.karlsson@cgi.com>
2022-12-01 18:51:36 +0100

    Day 1 complete

---
+++ AdventOfCode2022/Day/Day01.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day
+{
+    internal class Day01
+    {
+        private string _path;
+        public Day01(string path)
+        {
+            _path = Path.Combine(path, $"Input{this.GetType().Name}.txt");
+            PartOne();
+            PartTwo();
+        }
+        public void PartOne()
+        {
+            var result = File.ReadAllText(_path)
+                .Split("\r\n\r\n")
+                .Select(x => x.Split("\r\n").Sum(y => int.Parse(y)))
+                .Max();
+
+            Console.WriteLine($"Advent of Code Day 01 part 1 : {result}");
+        }
+
+        public void PartTwo()
+        {
+            var result = File.ReadAllText(_path)
+                .Split("\r\n\r\n")
+                .Select(x => x.Split("\r\n").Sum(y => int.Parse(y)))
+                .OrderByDescending(x => x)
+                .Take(3)
+                .Sum();
+
+            Console.WriteLine($"Advent of Code Day 01 part 2 : {result}");
+        }
+    }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCode2022.Day;
+
+namespace AdventOfCode2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine(Path.GetFullPath(@"..\..\..\"), "Inputs");
+
+            _ = new Day01(path);
+
+            Console.ReadLine();
+        }
+    }
+}
