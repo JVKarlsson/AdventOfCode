7fbb51e2858293a011cde9a51952d7906af1a8bc
Jimmy Karlsson <jimmy.karlsson@cgi.com>
2023-12-10 06:45:52 +0100

    Day 4

--- AdventOfCode2023/Day/Day04.cs
+++ AdventOfCode2023/Day/Day04.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AdventOfCode2023.Day;
@@ -17,13 +19,47 @@ public class Day04
 
     private void PartOne()
     {
-        var lines = File.ReadAllLines(_path);
-        Console.WriteLine(1);
+        var lines = File.ReadAllLines(_path)
+            .Select(x => x.Split(':')[1].Split('|').ToList());
+        
+        var count = 0;
+        foreach (var line in lines) 
+        {
+            var first = line[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+            var second = line[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            var test = first.Intersect(second).ToList();
+
+            if (test.Any())
+                count += (int)Math.Pow(2,(test.Count-1));
+        }
+        Console.WriteLine(count);
     }
 
     private void PartTwo()
     {
-        var lines = File.ReadAllLines(_path);
-        Console.WriteLine(1);
+        List<(int count, List<string> data)> lines = File.ReadAllLines(_path)
+            .Select(x => (1,x.Split(':')[1].Split('|').ToList())).ToList();
+
+        for (int i = 0; i < lines.Count; i++)
+        {
+            var line = lines[i];
+            var linecount = line.count;
+
+            var first = line.data[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+            var second = line.data[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            var matches = first.Intersect(second).ToList();
+            for (int k = 1; k <= matches.Count; k++)
+            {
+                if (i + k > lines.Count)
+                    break;
+
+                var temp = lines[i + k];
+                lines[i + k] = (temp.count + linecount, temp.data);
+            }
+        }
+        var sum = lines.Sum(x => x.count);
+        Console.WriteLine(sum);
     }
 }
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -9,8 +9,8 @@ internal class Program
         string path = Path.Combine(Path.GetFullPath(@"..\..\..\"), "Inputs");
         //_ = new Day01(path);
         //_ = new Day02(path);
-        _ = new Day03(path);
-        //_ = new Day04(path);
+        //_ = new Day03(path);
+        _ = new Day04(path);
         Console.ReadLine();
     }
 }
