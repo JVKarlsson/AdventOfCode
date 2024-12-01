c8a60db2d94be9b7b0d58a694a623ceb2c634894
Jimmy Karlsson <jimmy.karlsson@cgi.com>
2024-12-01 06:46:20 +0100

    2024 day 1

---
+++ AdventOfCode2024/Day/Day01.cs
@@ -0,0 +1,57 @@
+﻿namespace AdventOfCode2024.Day;
+
+internal class Day01
+{
+    private readonly string _path;
+    public Day01(string path)
+    {
+        _path = Path.Combine(path, $"Input{this.GetType().Name}.txt");
+        PartOne();
+        PartTwo();
+    }
+    private void PartOne()
+    {
+        var result = File.ReadAllLines(_path)
+                .Select(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+                .ToArray();
+
+        var list1 = new List<int>();
+        var list2 = new List<int>();
+        foreach (var item in result)
+        {
+            list1.Add(int.Parse(item[0]));
+            list2.Add(int.Parse(item[1]));
+        }
+        list1 = [.. list1.OrderBy(x => x)];
+        list2 = [.. list2.OrderBy(x => x)];
+
+        var listOfDiffs = new List<int>();
+        for (var i = 0; i < list1.Count; i++)
+        {
+            listOfDiffs.Add(Math.Abs(list2[i] - list1[i]));
+        }
+        var diff = listOfDiffs.Sum();
+
+        Console.WriteLine(diff);
+    }
+
+    private void PartTwo()
+    {
+        var result = File.ReadAllLines(_path)
+                .Select(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+                .ToArray();
+
+        var listOfDiffs = new List<int>();
+        var occurances = result.Select(x => int.Parse(x[1])).GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+        foreach (var item in result)
+        {
+            var value = int.Parse(item[0]);
+            var occurance = occurances.GetValueOrDefault(value, 0);
+            listOfDiffs.Add(value * occurance);
+        }
+
+        var diff = listOfDiffs.Sum();
+        Console.WriteLine(diff);
+    }
+
+}
---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,5 @@
+﻿using AdventOfCode2024.Day;
+
+string path = Path.Combine(Path.GetFullPath(@"..\..\..\"), "Inputs");
+_ = new Day01(path);
+Console.ReadLine();
